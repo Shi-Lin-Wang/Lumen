0bcb291780533ac76919c6aa80d5a874ff637a7a
Shi-Lin-Wang <44385829+Shi-Lin-Wang@users.noreply.github.com>
2020-07-09 03:30:08 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,6 @@ use Illuminate\Support\Facades\DB;
 |
 */
 //Route::view('/register', 'register')->name('register');
-/*Route::get('/123', function()
-{
-  // 測試一：取得users資料表的全部資料
-  $users = DB::table('account')->get();
-  return $users;
-
-  // 測試二：取得users資料表，id為1的資料
-  //$user = DB::table('users')->find(1);
-  //dd($user);  // dd means: die(var_dump($user));
-  //return $user->username;
-
-  // 測試三：用where條件式來取得相關資料
-  $users = DB::table('users')->where("username", "!=", "carlos")->get();
-  //也可使用：DB::select('select * from users');
-  return $users;
-});*/
 Route::get('/register', 'RegisterController@create');
 Route::post('/register', 'RegisterController@store');
 
@@ -70,47 +54,7 @@ Route::get('/welcome', function () {
 //Route::get('/chooseShop', 'chooseShopController@index')->name('chooseShop');
 
 });
-Route::get('/user', function () {
-$user = App\Register::all();
 
-foreach ($user as $user) {
-    echo $user->name;
-
-}});
-
-Route::get('/user/{id}', function ($id) {
-    $user = App\Register::find($id);
-    echo  $user->name;
-   });
-
-
-   Route::get('/email/{id}', function ($id) {
-    $user = App\Register::find($id);
-    echo  $user->email;
-   });
-
-Route::get('/update', function () {
-$user = App\Register::find(1); // 取得id = 1的資料
-$user->name = "Wang";
-$user->save();
-echo'更改成功';
-
-});
-
-
-Route::get('/data', function () {
-    return Register::all();
-
- });
-
- Route::get('/test', function () {
-    // res.header("Access-Control-Allow-Origin","*")
-    return view('personalInfo');
-
-});
-Route::post('/test', function () {
-    return view('personalInfo');
-});
 
 
 Route::get('/signup', 'signupController@create');
